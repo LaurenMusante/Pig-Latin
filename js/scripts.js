ce7593d5826862103d817194381bab02d8fc0461
Jason Huels, Jacqueline Remmel and Lauren Musante <jason@>
2019-08-21 11:14:56 -0700

    Add JS to move beginning consonant to end of word and add ay.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 function pigLatin(phrase) {
   const VOWELS = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
   var reg = /^[a-zA-Z]+$/g;
-
+  var letters = [];
 
     if(VOWELS.includes(phrase[0])) {
       phrase += "way";
     } else if(reg.test(phrase[0])) {
+      letters = phrase.split("");
+      letters.push(letters.splice(0, 1));
+      phrase = letters.join("");
       phrase += "ay";
-    }
+    } 
 
 
   console.log(phrase);

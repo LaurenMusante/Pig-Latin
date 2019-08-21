44ac3a29563e5e307bbaadcef97b6da5f4b1123b
Jason Huels, Jacqueline Remmel and Lauren Musante <lauren@>
2019-08-21 14:02:39 -0700

    Allow multi-word inputs to be translated.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,29 @@
-function pigLatin(phrase) {
+function pigLatin(input) {
   const VOWELS = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
-  var reg = /^[a-zA-Z]+$/g;
+  var reg = /^[a-zA-Z]/;
+  var words = [];
   var letters = [];
-
-    if(VOWELS.includes(phrase[0])) {
+  var output = "";
+  words = input.split(" ");
+  words.forEach(function(phrase) {
+    if(VOWELS.includes(phrase[0])) { //if the first letter of the word is a vowel
       phrase += "way";
-    } else if(reg.test(phrase[0])) {
+    } else if(reg.test(phrase[0])) { //if the first letter of the word is a consonant
       letters = phrase.split("");
       for (var i = 0; i < phrase.length; i++) {
-        if(!VOWELS.includes(phrase[i])) {
+        if(!VOWELS.includes(phrase[i])) {     //if the letter at index i is a consonant
           if(phrase[i].toLowerCase() === 'q' && phrase[i+1].toLowerCase() === 'u') {
-            letters.push(letters.splice(0, 1));
+            letters.push(letters.splice(0, 1)[0]);
           }
-            letters.push(letters.splice(0, 1));
+            letters.push(letters.splice(0, 1)[0]);
         } else {
-          break;
+          i = phrase.length;
         }
       }
       phrase = letters.join("");
       phrase += "ay";
     }
-  console.log(phrase);
+    output += phrase + " ";
+  });
+  console.log(output);
 }
